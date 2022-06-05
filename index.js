092760d864dd65f014b555c991ceef168944e929
Qowiyyu Adzkar <qowiyyuadzkar@gmail.com>
2022-06-05 23:57:30 +0700

    modifikasi function sorter, sortAscending, dan sortDescending

--- index.js
+++ index.js
@@ -6,15 +6,29 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (arrNama, cbSort) => {
+  let arrHasil = [];
+  let arrSort = cbSort(arrNama);
+
+  for (let index = 0; index < arrSort.length; index++) {
+    arrHasil.push((index+1)+ ". "+arrSort[index]);
+    
+  }
+
+  return arrHasil;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (arrNama) => {
+    return arrNama.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arrNama) => {
+  return arrNama.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
